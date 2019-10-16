2c121ca909e99cfed52b76be3f12d3a9a944bba4
Arkadiusz Kołodziejek <arkko002@gmail.com>
2019-10-16 14:46:55 +0200

    Change scraped_pages to hash

--- src/WebWalker/Website.cr
+++ src/WebWalker/Website.cr
@@ -1,9 +1,9 @@
 require "./Page"
 
 class Website
-    property scraped_pages : Array(Page)
+    property scraped_pages : Hash(String, Page)
 
     def initialize(@scraped_pages)
     end
-    
+
 end

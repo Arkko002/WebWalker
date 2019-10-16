14a34a361de0032748f79463f78aa15dce2501a8
Arkadiusz Kołodziejek <arkko002@gmail.com>
2019-10-16 14:47:26 +0200

    Add url refrence to Page, remove redundancy in initialize

--- src/WebWalker/Page.cr
+++ src/WebWalker/Page.cr
@@ -5,14 +5,13 @@ class Page
     property internal_links : Array(String)
     property external_links : Array(String)
     property visited : Bool
+    getter url : String
     getter http_response : HTTP::Client::Response
 
 
-    def initialize(http_response : HTTP::Client::Response)
+    def initialize(@http_response : HTTP::Client::Response, @url : String)
       @internal_links = Array(String).new
       @external_links = Array(String).new
       @visited = false
-
-      @http_response = http_response
     end
 end
